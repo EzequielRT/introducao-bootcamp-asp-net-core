9a08ca642687629af6f7569cb07efc4ca12e82c0
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-03-26 23:34:47 -0300

    Tipos de Dados e Interpolação

--- IntroducaoBootcampAspNetCore/Program.cs
+++ IntroducaoBootcampAspNetCore/Program.cs
@@ -6,7 +6,26 @@ namespace IntroducaoBootcampAspNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var umaString = "essa é uma string";
+            var numeroInt = 5;
+            var numeroFloat = 5.4f;
+            var numeroDouble = 5.4;
+            var numeroDecimal = 5.3m;
+            var array = new int[3] { 1, 2, 3 };
+            var caractere = 'a';
+            var data = new DateTime(2000, 11, 11);
+            var boolean = true;
+
+            Console.WriteLine($"{umaString}");
+            Console.WriteLine($"{numeroInt}");
+            Console.WriteLine($"{numeroFloat}");
+            Console.WriteLine($"{numeroDouble}");
+            Console.WriteLine($"{numeroDecimal}");
+            Console.WriteLine($"{array}");
+            Console.WriteLine($"{caractere}");
+            Console.WriteLine($"{data}");
+            Console.WriteLine($"{boolean}");
+
         }
     }
 }
