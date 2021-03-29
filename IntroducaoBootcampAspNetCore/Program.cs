955aed29f58d0bc39bd40106481c3f6ad7e20c0f
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-03-29 01:57:48 -0300

    POO - Abstracao, Encapsulamento, Heranca e Polimorfismo

--- IntroducaoBootcampAspNetCore/Program.cs
+++ IntroducaoBootcampAspNetCore/Program.cs
@@ -6,6 +6,7 @@ namespace IntroducaoBootcampAspNetCore
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
@@ -14,9 +15,149 @@ namespace IntroducaoBootcampAspNetCore
             //SwitchCase();
             //For();
             //While();
-            Lists();
+            //Lists();
+            ProgramacaoOrientadaAObjetos();
         }
 
+        #region ProgramacaoOrientadaAObjetos
+        public class Veiculo
+        {
+            public string Modelo { get; set; }
+            public string Marca { get; set; }
+            public int Ano { get; set; }
+            public decimal Valor { get; set; }
+            public string Placa { get; protected set; }
+
+            public virtual void MostrarRequisitosParaDirigir()
+            {
+                Console.WriteLine("Uma carteira de motorista.");
+            }
+
+        }
+
+        public class Onibus : Veiculo
+        {
+            public string LinhaDoOnibus { get; set; }
+
+            public override void MostrarRequisitosParaDirigir()
+            { 
+                Placa = "";
+                Console.WriteLine("Habilitação D.");
+            }
+        }
+
+        public class Moto : Veiculo
+        {
+            public string PropriedadeUnicaDeMoto { get; set; }
+
+            public override void MostrarRequisitosParaDirigir()
+            {
+                Console.WriteLine("Habilitação A.");
+            }
+        }
+
+        public class Carro : Veiculo
+        {
+            public override void MostrarRequisitosParaDirigir()
+            {
+                Placa = "";
+                Console.WriteLine("Habilitação B.");
+            }
+        }
+
+        public static void ProgramacaoOrientadaAObjetos()
+        {
+            var listaVeiculos = new List<Veiculo>();
+            listaVeiculos.Add(new Onibus());
+            listaVeiculos.Add(new Moto());
+            listaVeiculos.Add(new Carro());
+
+            foreach (var veiculo in listaVeiculos)
+            {
+                veiculo.MostrarRequisitosParaDirigir();
+            }
+
+            Console.WriteLine("---------- DevCars - Venda de Carros ----------");
+
+            ExibirMensagemPrincipal();
+
+            var opcao = Console.ReadLine();
+
+            while (opcao != "0")
+            {
+                switch (opcao)
+                {
+                    case "1":
+                        var veiculo = CadastrarVeiculo();
+                        listaVeiculos.Add(veiculo);
+                        break;
+                    case "2":
+                        ListarVeiculos(listaVeiculos);
+                        break;
+                    case "3":
+                        ComprarVeiculo(listaVeiculos);
+                        break;
+                    default:
+                        break;
+                }
+
+                ExibirMensagemPrincipal();
+                opcao = Console.ReadLine();
+            }
+        }
+
+        public static void ExibirMensagemPrincipal()
+        {
+            Console.WriteLine("Digite sua opção: ");
+            Console.WriteLine("1 - Cadastro");
+            Console.WriteLine("2 - Listagem");
+            Console.WriteLine("3 - Compra");
+            Console.WriteLine("0 - Sair");
+        }
+
+        public static Veiculo CadastrarVeiculo()
+        {
+            var veiculo = new Veiculo();
+
+            Console.WriteLine("Digite a Marca: ");
+            veiculo.Marca = Console.ReadLine();
+
+            Console.WriteLine("Digite o Modelo: ");
+            veiculo.Modelo = Console.ReadLine();
+
+            Console.WriteLine("Digite o Ano: ");
+            veiculo.Ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Valor: ");
+            veiculo.Valor = decimal.Parse(Console.ReadLine());
+
+            return veiculo;
+        }
+
+        public static void ListarVeiculos(List<Veiculo> veiculos)
+        {            
+            for (var i = 0; i < veiculos.Count; i++)
+            {
+                Console.WriteLine($"{i} - Marca: {veiculos[i].Marca}, Modelo: {veiculos[i].Modelo}, Ano: {veiculos[i].Ano}, Valor: R${veiculos[i].Valor}");
+            }
+        }
+
+        public static void ComprarVeiculo(List<Veiculo> veiculos)
+        {
+            ListarVeiculos(veiculos);
+
+            Console.WriteLine("Escolha uma opção de Veiculo: ");
+
+            var opcao = int.Parse(Console.ReadLine());
+
+            var veiculo = veiculos[opcao];
+
+            veiculos.Remove(veiculo);
+        }
+
+        #endregion
+
+        #region Lists
         public static void Lists()
         {
             var notasTurmas = new List<int> { 10, 5, 2, 3, 10, 4, 3, 2, 9, 10, 6, 7, 10, 2, 3, 4, 5, 7 };
@@ -36,6 +177,9 @@ namespace IntroducaoBootcampAspNetCore
             }
         }
 
+        #endregion
+
+        #region While
         public static void While()
         {
             var valores = Console.ReadLine(); // "1 2 3 4 5"
@@ -52,6 +196,9 @@ namespace IntroducaoBootcampAspNetCore
 
         }
 
+        #endregion
+
+        #region For
         public static void For()
         {
             var valores = Console.ReadLine(); // "1 2 3 4 5"
@@ -63,6 +210,9 @@ namespace IntroducaoBootcampAspNetCore
             }
         }
 
+        #endregion
+
+        #region SwitchCase
         public static void SwitchCase()
         {
             Console.WriteLine("Digite uma opção entre 1 e 3: ");
@@ -84,6 +234,9 @@ namespace IntroducaoBootcampAspNetCore
             }
         }
 
+        #endregion
+
+        #region ElseIf
         public static void ElseIf()
         {
             Console.WriteLine("Digite uma opção entre 1 e 3: ");
@@ -107,6 +260,9 @@ namespace IntroducaoBootcampAspNetCore
             }
         }
 
+        #endregion
+
+        #region TiposDeDados
         public static void TiposDeDados()
         {
             var umaString = "essa é uma string";
@@ -129,5 +285,7 @@ namespace IntroducaoBootcampAspNetCore
             Console.WriteLine($"{data}");
             Console.WriteLine($"{boolean}");
         }
+
+        #endregion
     }
 }
